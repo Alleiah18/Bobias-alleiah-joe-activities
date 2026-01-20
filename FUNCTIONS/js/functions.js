ea039a22ded8ac4de7a917e480190ad69198c76b
unknown <bobiasalleiahjoe@gmail.com>
2026-01-20 17:27:50 +0100

    FUNCTIONS

---
+++ FUNCTIONS/js/functions.js
@@ -0,0 +1,12 @@
+function normal() {
+    withParameter("Alleiah joe", "Bobias");
+}
+
+function withParameter(fname, lname) {
+    alert("Hello " + fname + " " + lname);
+}
+
+function getSum(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
